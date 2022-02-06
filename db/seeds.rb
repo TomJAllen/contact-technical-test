cd23d50a98c92d90e426e77f68efe503ada32abf
Tom Allen <tomallen_1@hotmail.co.uk>
2022-02-06 21:27:45 +0000

    created seed file that reads from data file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+
+Talent.destroy_all
+
+puts 'Creating db...'
+
+filepath = 'db/data.json'
+talent_json = File.read(filepath)
+talent_array = JSON.parse(talent_json)
+talent_array.each do |talents|
+  attributes = {
+    name: talents['name'],
+    location: talents['location'],
+    date_of_birth: talents['date_of_birth']
+  }
+  new_talent = Talent.create(attributes)
+  puts "Addded #{new_talent.name}"
+end
+puts 'Finished!'
